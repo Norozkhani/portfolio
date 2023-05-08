a32a186fc830de9f96342dd2f9ff95b881792b8b
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-05-08 14:56:00 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import "./index";
+import HomePage from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Works from "./components/Works";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
